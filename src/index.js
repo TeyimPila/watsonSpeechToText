20d7b6dcce0919a14c3dfef37281a22a4256afc2
Teyim Pila <teyim.pila@aun.edu.ng>
2018-01-13 20:34:36 +0100

    rename variable properly

--- src/index.js
+++ src/index.js
@@ -145,6 +145,10 @@ class App extends Component {
     }
 
 
+    getLatestResponse() {
+        const latest = this.state.formattedMessages[this.state.formattedMessages.length - 1];
+
+    }
 
 
 
@@ -155,73 +159,7 @@ class App extends Component {
 
 
 
-    /**
-     * This method retrieves an authorization token and 
-     * uses it to perfrom API calls for transction using audio and type keys
-     * provided in props object.
-     * 
-     * @param {audio: file, type: transcription type} props 
-     */
-    // transcribeAudio(props) {
 
-    //     if (props !== null && (props.type === 1 || props.type === 0)) {
-
-    //         if (this.state.stream) {
-    //             this.setState({ response: '', audio: null })
-    //             this.state.stream.stop()
-    //         }
-
-    //         let authorization = new AuthorizationV1({
-    //             username: USER_NAME,
-    //             password: PASSWORD,
-    //             url: SpeechToTextV1.URL
-    //         });
-
-    //         authorization.getToken(function (error, token) {
-    //             if (token) {
-    //                 if (props.type === 1) {
-    //                     stream = SpeechToText.recognizeMicrophone({
-    //                         token: token,
-    //                         model: 'en-US_NarrowbandModel',
-    //                         realtime: false,
-    //                         resultsBySpeaker: true,
-    //                     })
-    //                 } else {
-    //                     stream = SpeechToText.recognizeFile({
-    //                         token: token,
-    //                         file: props.audio,
-    //                         resultsBySpeaker: true,
-    //                         model: 'en-US_NarrowbandModel',
-    //                         realtime: false, // don't slow down the results if transcription occurs faster than playback
-    //                         play: true
-    //                     })
-    //                 }
-
-    //                 this.setState({ stream })
-
-    //                 stream.on('data', function (data) {
-    //                     // const text = data.toString()
-    //                     this.setState({ response: this.state.response += text })
-    //                 }.bind(this))
-
-    //             } else {
-    //                 console.log('Error: ', error);
-    //             }
-    //         }.bind(this));
-    //     }
-    // }
-
-
-    /**
-     * Stops the stream and reset state variables 
-     * when stop button is pressed.
-     */
-    // stopTranscription() {
-    //     if (this.state.stream) {
-    //         this.state.stream.stop()
-    //         this.setState({ audio: null, stream: null })
-    //     }
-    // }
 
     /**
      * Renders component on DOM
